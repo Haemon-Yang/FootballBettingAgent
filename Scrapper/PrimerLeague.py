eced32293da755cfc22e714b7f03d885b2a292d1
Haemon-Yang <haemon90013@gmail.com>
2025-03-20 16:05:39 +0800

    Revise scrapping Logic of PrimerLeague Scrapper

--- Scrapper/PrimerLeague.py
+++ Scrapper/PrimerLeague.py
@@ -4,7 +4,8 @@ import pandas as pd
 import numpy as np
 import requests
 import time
-from .scrapperData import Data
+#from .scrapperData import Data
+from scrapperData import Data
 import csv
 import os
 from io import StringIO
@@ -12,58 +13,58 @@ from io import StringIO
 class PremierLeagueCrawler:
    data = requests.get(Data.ScrappedUrl, headers = Data.get_headers())
    soup = BeautifulSoup(data.text, 'html.parser')
-   teamsUrl = []
-   teamsName = []
-   teamsInfo = {} # store Scores & Fixtures Dataframe
+   teamsInfo = {} # store Teams Name & url
 
    def __init__(self):
       pass
 
    # Update all teams
-   def scrap(self):
+   def scrap(self, url: str) -> list[pd.DataFrame]:
       try:
-         idx = 0
-         for url in PremierLeagueCrawler.teamsUrl:
-            delay = np.random.uniform(2, 5)
-            time.sleep(delay)
-            dataText = requests.get(url, headers=Data.get_headers())
-            matches = pd.read_html(StringIO(dataText.text), match="Scores & Fixtures")
-            PremierLeagueCrawler.teamsInfo.update({PremierLeagueCrawler.teamsName[idx]: matches})
-            idx += 1
-            self.saveTeams()
+         delay = np.random.uniform(2, 5)
+         time.sleep(delay)
+         dataText = requests.get(url, headers=Data.get_headers())
+         matches = pd.read_html(StringIO(dataText.text), match="Scores & Fixtures")
+         standardStats = pd.read_html(StringIO(dataText.text), match="Standard Stats")
+         playerShootingStats = pd.read_html(StringIO(dataText.text), match="Shooting")
+         playerPassingStats = pd.read_html(StringIO(dataText.text), match="Passing")
+         playerPassTypes = pd.read_html(StringIO(dataText.text), match="Pass Types")
+         playerGoalandShotCreaterion = pd.read_html(StringIO(dataText.text), match="Goal and Shot Creation")
+         playerDefensiveActions = pd.read_html(StringIO(dataText.text), match="Defensive Actions")
+         playerPossession = pd.read_html(StringIO(dataText.text), match="Possession")
+         playerPlayingTime = pd.read_html(StringIO(dataText.text), match="Playing Time")
+         playerMisc = pd.read_html(StringIO(dataText.text), match="Miscellaneous Stats")
+         return [matches, standardStats, 
+                 playerShootingStats, playerPassingStats, playerPassTypes, playerGoalandShotCreaterion, 
+                 playerDefensiveActions, playerPossession, playerPlayingTime, playerMisc]
+
       except Exception as e:
          print(f"Error in scrap: {e}")
-
-   def getTeams(self):
+   
+   def scrapSquadStats(self, url: str) -> list[pd.DataFrame]:
+      return []
+  
+   # Get all Teams Name & url
+   def getTeamsUrl(self):
       try:         
          # Select Table 
          premierLeague = PremierLeagueCrawler.soup.select(Data.targetTable)[0]
          # Select Link
          links = premierLeague.find_all("a")
-         links = [l.get("href") for l in links]
-         links = [l for l in links if '/squads/' in l]
-         PremierLeagueCrawler.teamsUrl = [f"https://fbref.com{l}" for l in links]
-         # Teams
-         for team in premierLeague.find_all("a"):
-            if '/squads/' in team.get("href"):
-               PremierLeagueCrawler.teamsName.append(team.text)
+
+         for link in links:
+            # Get Team Name
+            if '/squads/' in link.get("href"):
+               teamName = link.text
+            url = link.get("href")
+            if '/squads/' in url:
+               url = url
+               PremierLeagueCrawler.teamsInfo.update({teamName: f"https://fbref.com{url}"})
       except Exception as e:
          print(f"Error in getTeamsUrl: {e}")
 
-   def saveTeams(self):
-      try:
-         for team_name, team_data in self.teamsInfo.items():
-            fileName = f"{team_name}.csv"
-            filePath = os.path.join(Data.filePath, fileName)
-            
-            if isinstance(team_data[0], pd.DataFrame):
-               with open(filePath, 'w', newline='', encoding='utf-8') as f:
-                  team_data[0].to_csv(f, index=False)
-            else:
-               print(f"Warning: Data for {team_name} is not in DataFrame format")
-         
-      except Exception as e:
-         print(f"Error saving teams data: {e}")       
+   def uploadTeamsStatsToDatabase(self, teamsStats: list[pd.DataFrame]):
+      pass 
 
    def loadTeamsInfo(self, filePath: str) -> pd.DataFrame:
       if not os.path.exists(filePath):
@@ -79,6 +80,8 @@ class PremierLeagueCrawler:
    # ToDo: 1. async scrapping 2. store into database (local & cloud) 3. Load Data
    # Additional: 1. logging monitoring
 
-#if __name__ == "__main__":
-#   crawler = PremierLeagueCrawler()
-#   print(crawler.teamsName[0])
+if __name__ == "__main__":
+   crawler = PremierLeagueCrawler()
+   crawler.getTeamsUrl()
+   crawler.scrap()
+   print(crawler.teamsName[0])
